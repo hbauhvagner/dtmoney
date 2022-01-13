0bb19d35368faedfb2b1bffc3f1ef4bc11c1b236
hbauhvagner <hbauhvagner@gmail.com>
2022-01-12 23:51:03 -0300

    Finalizando inserção

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,10 +1,12 @@
+import { FormEvent, useState, useContext } from 'react';
+import { TransactionContext } from '../../TransactionsContext';
 import Modal from 'react-modal';
+
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 import closeImg from '../../assets/close.svg';
+
 import { Container, TransactionTypeContainer, RadioBox } from './styles';
-import { FormEvent, useState, useContext } from 'react';
-import { TransactionContext } from '../../TransactionsContext';
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -19,15 +21,21 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
     const [category, setCategory] = useState('');
     const [type, setType] = useState('deposit');
 
-    function handleCreateNewTransaction(event: FormEvent) {
+    async function handleCreateNewTransaction(event: FormEvent) {
         event.preventDefault();
 
-        createTransaction({
+        await createTransaction({
             title,
             amount,
             category,
             type
         })
+
+        setTitle('');
+        setAmount(0);
+        setCategory('');
+        setType('deposit');
+        onRequestClose();
     }
 
     return (
